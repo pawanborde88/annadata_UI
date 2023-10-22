ebb490bcac38bbbbac891663a728ecd453d8eedd
pawanborde88 <pavanborde1999@gmailcom>
2023-10-22 17:18:33 +0530

    fgg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ChipsModule } from 'primeng/chips';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChipsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
